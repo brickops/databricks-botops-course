d1cd236dd701b6fe4b8b54f5d65a5fd567d79a89
Pål de Vibe <123729504+paalvibeknowit@users.noreply.github.com>
2025-03-31 16:46:58 +0200

    initial fork from dataops

---
+++ orgs/acme/domains/transport/projects/taxinyc/flows/prep/dltrevenue/setupdb.py
@@ -0,0 +1,44 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Deploy jobs defined by deployment.yml
+
+# COMMAND ----------
+
+!pip install brickops=0.3.15
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ### Import libs
+
+# COMMAND ----------
+
+# Enable live reloading of libs, not needed now
+%load_ext autoreload
+%autoreload 2
+
+# COMMAND ----------
+
+# Name functions enables automatic env+user specific database naming
+from brickops.datamesh.naming import catname_from_path
+from brickops.datamesh.naming import dbname
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Setup Unity catalog database
+# MAGIC A Catalog will be created prefixed with username, branch and has commit
+
+# COMMAND ----------
+
+cat = catname_from_path()
+db = dbname(db="dltrevenue", cat=cat)
+print("New db name: " + db)
+spark.sql(f"USE catalog {cat}")
+spark.sql(f"CREATE DATABASE IF NOT EXISTS {db}")
+
+# COMMAND ----------
+
+
