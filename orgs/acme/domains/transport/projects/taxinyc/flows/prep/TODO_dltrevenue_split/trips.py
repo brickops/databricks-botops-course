c924a04f7f341e6c4b0550d8b4016920d24d9cbd
Pål de Vibe <123729504+paalvibeknowit@users.noreply.github.com>
2025-04-11 01:11:11 +0200

    cleanup

--- orgs/acme/domains/transport/projects/taxinyc/flows/prep/TODO_dltrevenue_split/trips.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-import dlt
-from pyspark.sql import functions as F
-
-
-@dlt.table
-def trips():
-    return spark.table("training.taxinyc_trips.yellow_taxi_trips_curated_sample")
-
